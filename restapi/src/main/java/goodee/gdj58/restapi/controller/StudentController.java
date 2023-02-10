d4614e3a71f7f31c64f63acb8a8efedd345b503b
GDJ58 <hyosang8823@daum.net>
2023-02-10 12:36:02 +0900

    그래프 커밋

---
+++ restapi/src/main/java/goodee/gdj58/restapi/controller/StudentController.java
@@ -0,0 +1,19 @@
+package goodee.gdj58.restapi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import goodee.gdj58.restapi.vo.Student;
+
+@Controller
+public class StudentController {
+	@GetMapping("/addStudent")
+	public String addStudent() {
+		return "addStudent";
+	}
+	@PostMapping("/addStudent")
+	public String addStudent(Student student) {
+		return "redirect:";
+	}
+}
